0ace5fce34b0f102d3d36a1d0c669d1288290a6b
Henry Tiria <htiria@todo1.net>
2022-03-16 13:09:42 -0500

    Estructura del proyecto

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PagesRoutingModule } from './pages-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+    declarations: [ProductListComponent, ProductDetailComponent, HomeComponent],
+    imports: [CommonModule, PagesRoutingModule, SharedModule],
+})
+export class PagesModule {}

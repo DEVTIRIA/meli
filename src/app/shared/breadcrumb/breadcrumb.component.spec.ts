7eda1f5c42411eef9132bbb0f026b90bcb6d9586
Henry Tiria <htiria@todo1.net>
2022-03-17 12:06:43 -0500

    Maqueta de las vistas y definición de rutas

--- src/app/shared/breadcrumb/breadcrumb.component.spec.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { BreadcrumbComponent } from './breadcrumb.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('BreadcrumbComponent', () => {
+  let component: BreadcrumbComponent;
+  let fixture: ComponentFixture<BreadcrumbComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ BreadcrumbComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(BreadcrumbComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '../components/components.module';
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent],
-    imports: [CommonModule],
-    exports: [HeaderComponent],
+    declarations: [HeaderComponent, BreadcrumbComponent],
+    imports: [ComponentsModule, RouterModule],
+    exports: [HeaderComponent, BreadcrumbComponent],
 })
 export class SharedModule {}

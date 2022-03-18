2c0e47e1fe123bfa186ea5d52596884f03b19f96
Henry Tiria <htiria@todo1.net>
2022-03-18 17:34:05 -0500

    Adding a service worker - PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,18 @@ import { AppComponent } from './app.component';
 
 import localeEs from '@angular/common/locales/es';
 import { LocationStrategy, PathLocationStrategy, registerLocaleData } from '@angular/common';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 registerLocaleData(localeEs, 'es');
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
     providers: [
         {
             provide: LocationStrategy,
